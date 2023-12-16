267f4c6ae3bb9944b30c59401371efcfa56a26b1
Zin_User <as7227020@gmail.com>
2023-12-16 16:57:57 +0800

    增加主題顏色 以及調整檔案路徑

--- MyPr/src/App.tsx
+++ MyPr/src/App.tsx
@@ -1,13 +1,14 @@
 
 import './App.css'
-import { AccordionDemo } from './components/ui/AccordionDemo'
+import { AccordionDemo } from '@/components/AccordionDemo'
+import { ThemeProvider } from "@/components/theme-provider"
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <AccordionDemo/>
-    </>
+    </ThemeProvider>
   )
 }
 
