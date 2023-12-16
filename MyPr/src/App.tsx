4a8a2a28e500ff89eb6667300f901abef433d235
Zin_User <as7227020@gmail.com>
2023-12-16 17:17:56 +0800

    增加react-icons替換shadcn BTN  以及安裝shadcn-ui 其他compent
    安裝shadcn-ui 其他compent時輸入 : npx shadcn-ui@latest add 即可

--- MyPr/src/App.tsx
+++ MyPr/src/App.tsx
@@ -2,11 +2,13 @@
 import './App.css'
 import { AccordionDemo } from '@/components/AccordionDemo'
 import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
 
 function App() {
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle/>
     <AccordionDemo/>
     </ThemeProvider>
   )
